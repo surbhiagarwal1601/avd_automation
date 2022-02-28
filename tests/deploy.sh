430d2d53ee8bcc530e93a2d0cf6daf0f8dd945c9
Brig Lamoreaux <bklamoreaux@gmail.com>
2022-02-28 16:19:42 -0700

    Test Role

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -14,5 +14,18 @@ storage_account_id=$(az storage account show --name $storage_name --query id -o 
 resourcegroupname=myAvdFromBash
 componentsStorageContainerName=components
 
-az deployment sub validate --location $location --template-file $template --parameters $parameters  resourcegroupname=$resourcegroupname componentStorageAccountId=$storage_account_id componentsStorageContainerName=$componentsStorageContainerName
-# az deployment sub create --location $location --template-file $template --parameters parameters 
+# Master Template
+az deployment sub create --location $location --template-file $template --parameters $parameters  resourcegroupname=$resourcegroupname componentStorageAccountId=$storage_account_id componentsStorageContainerName=$componentsStorageContainerName
+
+# # Master Template - Local Param file
+# az deployment sub create --location $location --template-file $template --parameters $parameters  resourcegroupname=$resourcegroupname componentStorageAccountId=$storage_account_id componentsStorageContainerName=$componentsStorageContainerName
+
+
+# Child Template
+# storage_endpoint=$(az storage account show --name $storage_name --query "primaryEndpoints.blob" -o tsv)
+# modules_path=$storage_endpoint$componentsStorageContainerName/Modules/ARM/
+
+# template_file=../Modules/ARM/RoleDefinitions/1.0.0/deploy.json
+
+# az deployment sub create --location westus2 --template-file ../Modules/ARM/RoleDefinitions/1.0.0/deploy.json   --parameters '@parameters.json' 
+
